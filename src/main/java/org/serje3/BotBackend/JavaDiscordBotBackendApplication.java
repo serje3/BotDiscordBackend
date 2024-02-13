1a8198dfa040ae42b5788175798f673ae5107e05
serje3 <serjerin322@gmail.com>
2024-02-13 15:43:14 +0300

    Initial commit

---
+++ src/main/java/org/serje3/BotBackend/JavaDiscordBotBackendApplication.java
@@ -0,0 +1,13 @@
+package org.serje3.BotBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDiscordBotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDiscordBotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/serje3/BotBackend/JavaDiscordBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serje3.BotBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDiscordBotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
