120fc015ababcaf9bbc155af301692f60bc9c0c1
serje3 <serjerin322@gmail.com>
2024-02-18 03:07:30 +0300

    Statistics endpoint

--- src/main/java/org/serje3/BotBackend/discord/members/MemberResource.java
+++ src/main/java/org/serje3/BotBackend/discord/members/MemberResource.java
@@ -1,10 +1,23 @@
 package org.serje3.BotBackend.discord.members;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.serje3.BotBackend.discord.events.log.EventLogService;
+import org.serje3.BotBackend.discord.members.data.MemberInfo;
+import org.serje3.BotBackend.domain.GuildId;
+import org.serje3.BotBackend.domain.UserId;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("members/")
+@RequiredArgsConstructor
 public class MemberResource {
 
+    private final MemberService memberService;
+
+    @GetMapping("/{userId}/info")
+    public ResponseEntity<MemberInfo> getMemberInfo(@PathVariable("userId") UserId userId){
+        return ResponseEntity.ok().body(memberService.getMemberInfo(userId));
+    }
 }
