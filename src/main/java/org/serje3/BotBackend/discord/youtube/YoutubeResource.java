013044d3f1f0dd4b1f544681264a5fc6a86d7dfc
serje3 <serjerin322@gmail.com>
2024-03-30 01:53:10 +0300

    feat: text sentry

--- src/main/java/org/serje3/BotBackend/discord/youtube/YoutubeResource.java
+++ src/main/java/org/serje3/BotBackend/discord/youtube/YoutubeResource.java
@@ -1,5 +1,7 @@
 package org.serje3.BotBackend.discord.youtube;
 
+import io.sentry.Hint;
+import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
@@ -17,5 +19,16 @@ public class YoutubeResource {
     @GetMapping("search")
     public ResponseEntity<?> search(@RequestParam("q") @NonNull String q) throws GeneralSecurityException, IOException {
         return ResponseEntity.ok(youtubeService.search(q));
+    }
+
+
+    @GetMapping("message")
+    public ResponseEntity<?> message(String text){
+        try {
+            int x = 23 / 0;
+        } catch (Exception e) {
+            Sentry.captureMessage(text);
+        }
+        return ResponseEntity.ok().build();
     }
 }
